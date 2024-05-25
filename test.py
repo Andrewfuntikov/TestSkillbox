5260e537c0745525e98ab4890852d5685cc6ab18
Андрей <andhunter77@gmail.com>
2024-05-25 16:26:24 +0500

    Сделай PEP8

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 print('Это тест на проверку работы Git  Github!')
-
+print('Принято    на другом пк')
 print('Принято на другом пк')
-print('Принято на другом пк')
-# TODO сделай pull
 print('Принято на Linux!')
+num = 1234657890
+print(id(num))
+for i in range(100):
+    print(id(num))
+    num = 1234657890
+    print(id(num))
+    num = 1234657890
+    print(id(num))
+# TODO испытай PEP8 Alt + Shift + F
