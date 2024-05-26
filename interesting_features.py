01b029468877d5ce6d817209fa2ee1a47b521431
AndHunter <sergeevandrej7824@gmail.com>
2024-05-26 21:00:25 +0500

    Добавил скачку с ютуба

--- interesting_features.py
+++ interesting_features.py
@@ -6,3 +6,10 @@ def my_function():
     print(123)
 
 print(my_function.__doc__)
+
+import pytube
+
+link = 'https://youtu.be/vQ2A6foqn38?si=rIcj4TfyWeRfEwR0'
+link = pytube.YouTube(link)
+video = link.streams.get_highest_resolution()
+video.download()
