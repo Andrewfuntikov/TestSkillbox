e8559b88c6a8cf09133dc0a3c539c128935e93e3
Андрей <andhunter77@gmail.com>
2024-05-27 20:58:21 +0500

    Сделал кое-что завтра доделать! Задания даны

--- card_game.py
+++ card_game.py
@@ -7,18 +7,15 @@ cards = ['Черви_6', 'Черви_7', 'Черви_8', 'Черви_9', 'Чер
          'Крести_Туз',
          'Вини_6', 'Вини_7', 'Вини_8', 'Вини_9', 'Вини_10', 'Вини_Валет', 'Вини_Дама', 'Вини_Король', 'Вини_Туз']
 
-
-def card_del():
-    for i in range(6):
-        cards.remove(card_player[i])
-        cards.remove(card_pc[i])
-
-
 card_player = random.sample(cards, 6)
 card_pc = random.sample(cards, 6)
-print(f'Ваша карта: {card_player}')
-if card_player == card_pc:
-    print('Ошибка!')
+
+
+def error():
+    if card_player == card_pc:
+        print('Техническая ошибка! \n Перезапустите игру!\n Код ошибки: 1')
+
+
 dict_card = {'Черви_6': 100,
              'Черви_7': 110,
              'Черви_8': 120,
@@ -62,29 +59,80 @@ print(f'Козырная масть: {random_mast}')
 
 meaning_card_player = 0
 meaning_card_pc = 0
+win_pc = 0
+win_player = 0
+round_number = 1
 
-for i in range(6):
-    if random_mast == card_player[i][0:5]:  # Проверка для червей
-        meaning_card_player += dict_card[card_player[i]] + 1000
-    elif random_mast == card_player[i][0:4]:
-        meaning_card_player += dict_card[card_player[i]] + 1000
-    elif random_mast == card_player[i][0:6]:
-        meaning_card_player += dict_card[card_player[i]] + 1000
-    else:
-        meaning_card_player += dict_card[card_player[i]]
+while len(card_player) > 0 and len(card_pc) > 0:
+    try:
+        error()
+        print(f'Раунд: {round_number}')
+        round_number += 1
+        print(f'В колоде осталось {len(cards)} карт.')  # Печатаем количество карт в колоде
+        print('Ваши карты: ', card_player)
+        choice_player = input('Введите карту которой хотите сходить: ')
+        while choice_player not in card_player:  # Проверка на ввод карты, которой нет у игрока
+            print('У вас нет такой карты!')
+            choice_player = input('Введите карту которой хотите сходить: ')
+        choice_pc = random.choice(card_pc)
+        card_player.remove(choice_player)  # Удаление карты игрока из списка
+        card_pc.remove(choice_pc)  # Удаление карты компьютера из списка
+        cards.remove(choice_player)  # Удаление карты из колоды
+        cards.remove(choice_pc)  # Удаление карты из колоды
+        if random_mast == choice_player[0:5]:  # Проверка для червей
+            meaning_card_player += dict_card[choice_player] + 1000
+        elif random_mast == choice_player[0:4]:
+            meaning_card_player += dict_card[choice_player] + 1000
+        elif random_mast == choice_player[0:6]:
+            meaning_card_player += dict_card[choice_player] + 1000
+        else:
+            meaning_card_player += dict_card[choice_player]
 
-    if random_mast == card_pc[i][0:5]:  # Проверка для червей
-        meaning_card_pc += dict_card[card_pc[i]] + 1000
-    elif random_mast == card_pc[i][0:4]:
-        meaning_card_pc += dict_card[card_pc[i]] + 1000
-    elif random_mast == card_pc[i][0:6]:
-        meaning_card_pc += dict_card[card_pc[i]] + 1000
-    else:
-        meaning_card_pc += dict_card[card_pc[i]]
+        if random_mast == choice_pc[0:5]:  # Проверка для червей
+            meaning_card_pc += dict_card[choice_pc] + 1000
+        elif random_mast == choice_pc[0:4]:
+            meaning_card_pc += dict_card[choice_pc] + 1000
+        elif random_mast == choice_pc[0:6]:
+            meaning_card_pc += dict_card[choice_pc] + 1000
+        else:
+            meaning_card_pc += dict_card[choice_pc]
 
-if meaning_card_player > meaning_card_pc:
-    print('Пользователь победил')
-elif meaning_card_player == meaning_card_pc:
-    print('Одинаковые значения карт == недопустимо')
+        print(f'Игрок ходит картой {choice_player}, а компьютер {choice_pc}:')
+
+        if meaning_card_player > meaning_card_pc:
+            print('Пользователь выйграл этот раунд.')
+            win_player += 1
+        elif meaning_card_player == meaning_card_pc:
+            print('Одинаковые значения карт == недопустимо.')
+        elif meaning_card_player > meaning_card_pc:
+            print('Компьютер выйграл этот раунд.')
+            win_pc += 1
+        else:
+            ('Пользователь проиграл.')
+            win_pc += 1
+
+        # Пополнение карт до 6
+        if len(card_player) < 6:
+            card_player.extend(random.sample(cards, 6 - len(card_player)))
+        if len(card_pc) < 6:
+            card_pc.extend(random.sample(cards, 6 - len(card_pc)))
+    except:
+        continue
+        # Пополнение карт до 6
+        if len(card_player) < 6:
+            card_player.extend(random.sample(cards, 6 - len(card_player)))
+        if len(card_pc) < 6:
+            card_pc.extend(random.sample(cards, 6 - len(card_pc)))
+
+if win_pc > win_player:
+    print('Увы.... \n Компьютер выйграл.')
 else:
-    print('Пользователь проиграл')
+    print('Вы выйграли поздравляю!')
+
+# TODO добавить удаление карт из списка до цикла
+# TODO добавить коректировки
+# TODO добавить try обработчик ошибок
+# TODO разобраться с проблемой в конце игры
+# TODO Откоректировать текст
+# TODO откоректирвоать error()
+# TODO спрыгнуть с крыши
