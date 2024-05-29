a97aca31b8ca45b9378532e964c6582ae7b471fd
Андрей <andhunter77@gmail.com>
2024-05-29 23:29:21 +0500

    Изменения

--- TESTS/probe.py
+++ TESTS/probe.py
@@ -1,3 +1,11 @@
-import tests
-print(tests.variable_1)
-tests.function1()
+def func_2():
+    def func_3():
+        print('in func_3 x =', x)
+
+    print('in func_2 x =', x)
+    func_3()
+
+
+x = 42
+func_2()
+print('x =', x)
