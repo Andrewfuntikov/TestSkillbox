bf2f3ebdcc3e0aa75a8fabcc4c02638d10413d9f
Андрей <andhunter77@gmail.com>
2024-05-30 09:56:36 +0500

    Тесты

--- TESTS/probe.py
+++ TESTS/probe.py
@@ -1,11 +1,35 @@
-def func_2():
-    def func_3():
-        print('in func_3 x =', x)
+from random import randint
 
-    print('in func_2 x =', x)
-    func_3()
+MAX_BUNCHES = 5
+MAX_BUNCHE_SIZE = 20
+
+_holder = {}
+_sorted_keys = None
 
 
-x = 42
-func_2()
-print('x =', x)
+def put_stones():
+    """ расположить камни на игровой поверхности """
+    global _holder, _sorted_keys
+    _holder = {}
+    for i in range(1, MAX_BUNCHES + 1):
+        _holder[i] = randint(1, MAX_BUNCHE_SIZE)
+    _sorted_keys = sorted(_holder.keys())
+
+
+def take_from_bunch(position, quantity):
+    if position in _holder:
+        _holder[position] -= quantity
+        return True
+    else:
+        return False
+
+
+def get_bunches():
+    res = []
+    for key in _sorted_keys:
+        res.append(_holder[key])
+    return res
+
+
+def is_gameover():
+    return sum(_holder.values()) == 0
