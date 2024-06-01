eeaae3ef8dc563da64ce2be6f6a22532bce28296
Андрей <andhunter77@gmail.com>
2024-06-01 10:14:04 +0500

    Функции Классов

--- Инструкции/Функции!.py
+++ Инструкции/Функции!.py
@@ -22,3 +22,46 @@
 # _sorted_keys = None Подчерк перед переменной говорит о том внутреняя переменная которую использовать на прямую не надо
 # hasattr() - проверяем если ли атрибут в обьекте
 # setattr() - проверяем если ли атрибут в обьекте, но можно манипулировать обьектами как переменной
+# Эмуляция операторов сравнения
+#
+# object.__eq__(self, other) - равенство двух объектов ==
+# object.__ne__(self, other) - не равно !=
+# object.__lt__(self, other) - строго меньше <
+# object.__le__(self, other) - меньше или равно <=
+# object.__gt__(self, other) - строго больше >
+# object.__ge__(self, other) - больше или равно >=
+#
+# должны возвращать boolean - True/False
+
+# Эмуляция математических операций
+# 2 + 2
+# my_car + truck
+#
+# object.__add__(self, other) - сложение +
+# object.__sub__(self, other) - вычитание -
+# object.__mul__(self, other) - умножение *
+# object.__truediv__(self, other) - деление /
+# object.__floordiv__(self, other) - целочисленное деление //
+# object.__mod__(self, other) - остаток от деления %
+# object.__pow__(self, other) - возведение в степень **
+# object.__lshift__(self, other) - побитовый сдвиг влево <<
+# object.__rshift__(self, other) - побитовый сдвиг вправо >>
+# object.__and__(self, other) - побитовое И &
+# object.__xor__(self, other) - побитовое исключающее ИЛИ ^
+# object.__or__(self, other) - побитовое ИЛИ |
+
+# для операций расширенного присвоения служат методы
+# object.__iadd__(self, other) - +=
+# object.__isub__(self, other) - -=
+# object.__imul__(self, other) - *=
+# object.__itruediv__(self, other) - /+
+# object.__ifloordiv__(self, other) - //=
+# object.__imod__(self, other) - %=
+# object.__ipow__(self, other) - **=
+# object.__ilshift__(self, other) - <<=
+# object.__irshift__(self, other) - >>=
+# object.__iand__(self, other) - &=
+# object.__ixor__(self, other) - ^=
+# object.__ior__(self, other) - |=
+#
+# они изменяют сам объект (по месту, inplace)
